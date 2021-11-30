80e75dd73165a510032c3c17b4ce9502aceceb01
davisgon <davisgon@gmail.com>
2021-11-30 13:04:27 -0500

    act
    act

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -7,12 +7,15 @@ import ProductItem from "./ProductItem";
 
 const ShoppingCart = function () {
     const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
-    const { products, cart } = state;
+    const { products, cart, totalCart, totAmount } = state;
+    let type = 'dec';
 
     const addToCart = (id) => {
-        console.log(id);
+        // console.log("id:"+id);
         dispatch({ type: TYPES.ADD_TO_CART, payload: id });
-       
+        dispatch({ type: TYPES.ADD_TOT_CART });
+        dispatch({ type: TYPES.TOGGLE_AMOUNT });
+
     };
 
     const delFromCart = (id, all = false) => {
@@ -21,6 +24,8 @@ const ShoppingCart = function () {
             dispatch({ type: TYPES.REMOVE_ALL_FROM_CART, payload: id });
         } else {
             dispatch({ type: TYPES.REMOVE_ONE_FROM_CART, payload: id });
+            dispatch({ type: TYPES.REMOVE_TOT_CART });
+           
         }
     };
 
@@ -31,17 +36,17 @@ const ShoppingCart = function () {
     return (
         <>
             <table>
-                <h2>Shopping Cart</h2>
                 <tr>
                     <th style={{ width: '1200px' }}>
                         <h3>Products</h3>
                     </th>
                     <th>
-                        <h3>Cart</h3>
-                        <button onClick={clearCart}>Clear Cart</button>
+                        <button onClick={clearCart}>Clear Cart --></button>
+                         {totalCart}
+                        <br />
+                      
                     </th>
                 </tr>
-
                 <td>
                     <article className="box grid-responsive">
                         {products.map((product) => (
@@ -49,8 +54,6 @@ const ShoppingCart = function () {
                         ))}
                     </article>
                 </td>
-
-
                 <td>
                     <article className="box2  grid-responsive">
                         {cart.map((item, index) => (
